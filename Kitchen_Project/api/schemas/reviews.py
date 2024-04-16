5df477609baad067ea80f4f05b84479140c7ce9a
Thomas Fletcher <159389948+tfletch8@users.noreply.github.com>
2024-04-16 13:02:43 -0400

    feat: added reviews.py

---
+++ Kitchen_Project/api/schemas/reviews.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class ReviewsBase(BaseModel):
+    customer_id: Integer
+    order_id: Integer
+    rating: Integer
+    feedback: Text
+
+
+class ReviewsCreate(ReviewsBase):
+    pass
+
+
+class ReviewsUpdate(BaseModel):
+    customer_id: Optional[Integer] = None
+    order_id: Optional[Integer] = None
+    rating: Optional[Integer] = None
+    feedback: Optional[Text] = None
+
+
+class Reviews(ReviewsBase):
+    id: int
+
+    class Config:
+        orm_mode = True
