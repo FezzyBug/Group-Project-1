d43900a2dace8053a17560bfdd46599f1bcdbdb6
Thomas Fletcher <159389948+tfletch8@users.noreply.github.com>
2024-04-16 12:52:20 -0400

    feat: added menu_items.py

---
+++ Kitchen_Project/api/schemas/menu_items.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class MenuItemsBase(BaseModel):
+    dish_name: str
+    description: str
+    price: DECIMAL
+    ingredients: Text
+    category: str
+
+
+class MenuItemsCreate(MenuItemsBase):
+    pass
+
+
+class MenuItemsUpdate(BaseModel):
+    dish_name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[DECIMAL] = None
+    ingredients: Optional[Text] = None
+    category: Optional[str] = None
+
+
+class MenuItems(MenuItemsBase):
+    id: int
+
+    class Config:
+        orm_mode = True
