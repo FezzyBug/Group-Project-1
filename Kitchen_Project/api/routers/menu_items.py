70c8e33e856314a4302068a26cc06471df62f3ec
Thomas Fletcher <159389948+tfletch8@users.noreply.github.com>
2024-04-24 17:26:32 -0400

    feat: Updated menu_items.py

--- Kitchen_Project/api/routers/menu_items.py
+++ Kitchen_Project/api/routers/menu_items.py
@@ -12,30 +12,30 @@ router = APIRouter(
 
 
 @router.post("/", response_model=schema.MenuItems)
-def create_promotion(request: schema.MenuItemsCreate, db: Session = Depends(get_db)):
-    """Create a new promotion."""
+def create_menuitems(request: schema.MenuItemsCreate, db: Session = Depends(get_db)):
+    """Create a new menu item."""
     return controller.create_menuitem(db=db, request=request)
 
 
 @router.get("/", response_model=List[schema.MenuItems])
-def read_all_promotions(db: Session = Depends(get_db)):
-    """Read all promotions."""
+def read_all_menuitems(db: Session = Depends(get_db)):
+    """Read all menu itemss."""
     return controller.read_all_menuitems(db)
 
 
-@router.get("/{promotion_id}", response_model=schema.MenuItems)
-def read_promotion(menuitem_id: int, db: Session = Depends(get_db)):
-    """Read a promotion by ID."""
+@router.get("/{menuitem_id}", response_model=schema.MenuItems)
+def read_menuitem(menuitem_id: int, db: Session = Depends(get_db)):
+    """Read a menu item by ID."""
     return controller.read_menuitem(db, menuitem_id=menuitem_id)
 
 
-@router.put("/{promotion_id}", response_model=schema.MenuItems)
-def update_promotion(menuitem_id: int, request: schema.MenuItemsUpdate, db: Session = Depends(get_db)):
-    """Update a promotion by ID."""
+@router.put("/{menuitem_id}", response_model=schema.MenuItems)
+def update_menuitem(menuitem_id: int, request: schema.MenuItemsUpdate, db: Session = Depends(get_db)):
+    """Update a menu item by ID."""
     return controller.update_menuitem(db=db, request=request, menuitem_id=menuitem_id)
 
 
-@router.delete("/{promotion_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_promotion(menuitem_id: int, db: Session = Depends(get_db)):
-    """Delete a promotion by ID."""
+@router.delete("/{menuitem_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_menuitem(menuitem_id: int, db: Session = Depends(get_db)):
+    """Delete a menu item by ID."""
     return controller.delete_menuitem(db=db, menuitem_id=menuitem_id)
