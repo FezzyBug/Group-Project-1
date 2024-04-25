f0a548ad0f09e76fd8a83043fb7e1ef3a2d79e4c
Thomas Fletcher <159389948+tfletch8@users.noreply.github.com>
2024-04-25 12:20:04 -0400

    feat: Updated index.py
    Included routers for reviews, menu items, and data analysis tables.

--- Kitchen_Project/api/routers/index.py
+++ Kitchen_Project/api/routers/index.py
@@ -1,7 +1,10 @@
-from . import orders, order_details, customers,promotions
+from . import orders, order_details, customers, promotions, menu_items, reviews, data_analysis
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(customers.router)
     app.include_router(promotions.router)
+    app.include_router(menu_items.router)
+    app.include_router(reviews.router)
+    app.include_router(data_analysis.router)
